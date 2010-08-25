662f1e3fe0392031927cf4952e2fff010d1fca7d
Juozas Kaziukenas <juozas@juokaz.com>
2010-08-25 19:03:19 +0300

    Improved IP lookup to handle errors gracefuly

--- Regitra/Util/Http.php
+++ Regitra/Util/Http.php
@@ -11,6 +11,22 @@ class Http
      */
     public static function getIp()
     {
-        return \trim(\file_get_contents('http://whatismyip.org/'));
+        static $ipAddress;
+
+        try
+        {
+            if (false === ($ip = @\file_get_contents('http://whatismyip.org/')))
+            {
+                throw new \Regitra\Exception('IP address cannot be retrieved');
+            }
+
+            $ipAddress = $ip;
+        }
+        catch (\Regitra\Exception $e)
+        {
+            $ip = $ipAddress;
+        }
+
+        return \trim($ip);
     }
 }
